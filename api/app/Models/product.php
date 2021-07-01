05e18cec8bf4424652185adbd14403415a407799
willian95 <rodriguezwillian95@gmail.com>
2021-07-01 06:42:16 -0400

    cambios

--- api/app/Models/product.php
+++ api/app/Models/product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
-class product extends Model
+class Product extends Model
 {
     use HasFactory;
     use SoftDeletes;
